5d187987cea7fd40dea96139bb30d45fcd399e55
Shaheen-Rajith <shaheen.rajith@outlook.in>
2024-05-25 14:47:52 +0400

    Added Hello World but in C

---
+++ hello2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello World! But in C this time\n");
+    printf("Enter a Number: ");
+    int num;
+    scanf("%d",&num);
+    printf("\nThe Number you typed was %d",num);
+    return 0;
+}
